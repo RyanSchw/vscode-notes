22dcd8fecd9fde02548f2cd7f9eabbe41923a093
Ryan Schwartz <32225075+RyanSchw@users.noreply.github.com>
2021-06-25 20:37:38 -0400

    Add configuration for notes directory

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('notes.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-notes!');
+		vscode.window.showInformationMessage(`Hello World from vscode-notes! [[${vscode.workspace.getConfiguration('notes').get('notesDirectory')}]]`);
 	});
 
 	context.subscriptions.push(disposable);
