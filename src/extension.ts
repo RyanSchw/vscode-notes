4bea1872b42fefc60e8e1bb156655ad72f577f47
Ryan Schwartz <32225075+RyanSchw@users.noreply.github.com>
2021-06-25 20:59:23 -0400

    Fix formatting from tab to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -5,39 +5,39 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "notes" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('notes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage(`Hello World from vscode-notes! [[${vscode.workspace.getConfiguration('notes').get('notesDirectory')}]]`);
-	});
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "notes" is now active!');
 
-	let directory = vscode.commands.registerCommand('notes.setDirectory', async () => {
-		try {
-			const fileUriArray = await vscode.window.showOpenDialog({
-				canSelectFiles: false,
-				canSelectFolders: true,
-				canSelectMany: false
-			});
-			if (fileUriArray !== undefined) {
-				const filePath = fileUriArray[0].path;
-				vscode.workspace.getConfiguration('notes').update('notesDirectory', filePath, true);
-			}
-		} catch (error) {
-			vscode.window.showErrorMessage('Something went wrong while opening the file browser.');
-		}
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('notes.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage(`Hello World from vscode-notes! [[${vscode.workspace.getConfiguration('notes').get('notesDirectory')}]]`);
+    });
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(directory);
+    let directory = vscode.commands.registerCommand('notes.setDirectory', async () => {
+        try {
+            const fileUriArray = await vscode.window.showOpenDialog({
+                canSelectFiles: false,
+                canSelectFolders: true,
+                canSelectMany: false
+            });
+            if (fileUriArray !== undefined) {
+                const filePath = fileUriArray[0].path;
+                vscode.workspace.getConfiguration('notes').update('notesDirectory', filePath, true);
+            }
+        } catch (error) {
+            vscode.window.showErrorMessage('Something went wrong while opening the file browser.');
+        }
+    });
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(directory);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
